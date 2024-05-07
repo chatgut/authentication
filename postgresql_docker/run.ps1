17801a5d871a2793bb515bcbecc281162c0be3ef
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2024-05-07 15:54:40 +0200

    added jager for jwt service

--- postgresql_docker/run.ps1
+++ postgresql_docker/run.ps1
@@ -1,3 +1,3 @@
-docker network create --driver bridge imageapi-net
+docker network create --driver bridge auth-net
 
 docker run -dit --memory 128m --network auth-net --name postgress-database -e POSTGRES_PASSWORD=root -p 5432:5432 app/database-postgresql
